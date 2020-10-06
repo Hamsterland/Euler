d676f447223c59bf91bd0de87ea4c46d5ba3f4d7
Hamster <57412787+Hamsterland@users.noreply.github.com>
2020-10-06 22:09:24 +0100

    Create 004.rb

---
+++ rb/004.rb
@@ -0,0 +1,27 @@
+# Solution to Project Euler Problem 4
+# Copyright (C) Hamsterland. All rights reserved.
+# https://projecteuler.net/problem=4
+
+def compute
+    ans = 0 
+    for a in 100..999
+        for b in 100..999
+            ab = a * b  
+            ans = ab if palindrome? ab and ab > ans 
+        end
+    end
+    ans
+end
+
+def palindrome? n 
+    str = n.to_s 
+    rev = str.reverse 
+    return true if str == rev 
+end 
+
+if $0 == __FILE__
+    puts compute
+end 
+
+
+ 
