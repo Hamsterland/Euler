a11c91b9ecdf9bf0915ac2b5a3bbea63da977f2d
Hamster <57412787+Hamsterland@users.noreply.github.com>
2020-10-07 11:56:00 +0100

    Create 005.rb

---
+++ rb/005.rb
@@ -0,0 +1,25 @@
+# Solution to Project Euler Problem 5
+# Copyright (C) Hamsterland. All rights reserved.
+# https://projecteuler.net/problem=5
+
+def divisible min, max, num 
+    divisors = min..max
+    for i in divisors
+        if num % i != 0
+            return false 
+        end
+    end
+    return true
+end
+
+def compute 
+    ans = 20
+    until divisible 11, 19, ans 
+        ans += 20
+    end
+    ans
+end 
+
+if $0 == __FILE__
+    puts compute
+end 
